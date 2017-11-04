fb08bcfd1ea711877cc5d079847ce255dad9ac40
GroupSSeS <33368894+GroupSSeS@users.noreply.github.com>
2017-11-04 13:19:54 +0100

    Revert "Initial commit"
    This reverts commit 024832f233218ac3ee6d19693a31b3e09d2c91c1.

--- User/log_out.php
+++
@@ -1,7 +0,0 @@
-<?php
-//questa funzione la si mette sempre prima di utilizzare i metodi per la sessione
-session_start();
-//quando facciamo il log-out cancelliamo la sessione.
-session_destroy();
-header("location:..\index.php?logout=1");
-?> 
